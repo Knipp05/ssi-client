3c268cdc6017d63631132da44b671d2ee3884145
nknipper <niklas.knipper@stud.htwk-leipzig.de>
2024-11-16 17:01:57 +0100

    changed localhost to local ip & added .gitignore

--- src/components/WalletConnection.tsx
+++ src/components/WalletConnection.tsx
@@ -1,15 +1,12 @@
-import React from 'react';
-import {QRCodeSVG} from 'qrcode.react';
+import React from "react";
+import { QRCodeSVG } from "qrcode.react";
 
 export default function WalletConnection() {
-    const vdAgentUrl = 'http://localhost:3001/issue-credential'
-    return(
-        <div>
-            <h2>QR-Code scannen, um die Wallet zu verbinden!</h2>
-            <QRCodeSVG
-            value={vdAgentUrl}
-            size={128}
-            />
-        </div>
-    )
+  const vdAgentUrl = "http://192.168.178.69:3001/issue-credential";
+  return (
+    <div>
+      <h2>QR-Code scannen, um die Wallet zu verbinden!</h2>
+      <QRCodeSVG value={vdAgentUrl} size={128} />
+    </div>
+  );
 }
